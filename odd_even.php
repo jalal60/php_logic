09807fd4511cf4d54c75a99441e74e62df45030d
Md. Shahjalal <jalal2k4@gmail.com>
2023-09-13 15:08:34 +0600

    Create odd_even.php

---
+++ odd_even.php
@@ -0,0 +1,14 @@
+<?php
+// ODD EVEN Check by Moduler Remainder % 
+$number=43;
+$remainder=$number%2;
+
+if($remainder==0){
+  echo "The number $number is Even";
+}
+else
+{
+  echo "The number $number is Odd";
+}
+
+?>
