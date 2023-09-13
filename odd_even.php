1e44a1a71a30fb8a275acb608530b6db2838fc6f
Md. Shahjalal <jalal2k4@gmail.com>
2023-09-13 15:26:39 +0600

    Update odd_even.php
    0 Zero Condition Check...

--- odd_even.php
+++ odd_even.php
@@ -1,14 +1,17 @@
 <?php
 // ODD EVEN Check by Moduler Remainder % 
-$number=43;
+$number=0;
 $remainder=$number%2;
 
-if($remainder==0){
+if($number==0)
+{
+  echo "The number is Zero";
+}
+else if($remainder==0){
   echo "The number $number is Even";
 }
 else
 {
   echo "The number $number is Odd";
 }
-
 ?>
