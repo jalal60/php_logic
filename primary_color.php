30df1b46d5f71a2497384595004f21a6fd511892
Md. Shahjalal <jalal2k4@gmail.com>
2023-09-15 14:18:07 +0600

    Added Primary Color Checker

---
+++ primary_color.php
@@ -0,0 +1,10 @@
+<?php
+$color = "white";
+
+if ($color == "yelow" || $color == "blue" || $color == "red") {
+    echo "Primary Color";
+} else if ($color == "green" || $color == "purple") {
+    echo "Secondary Color";
+} else {
+    echo "Orther Colors";
+}
