247dcbf3aac9d23f7ce48d3191543ad9717291e2
Md. Shahjalal <jalal2k4@gmail.com>
2023-09-23 21:22:10 +0600

    Update array_exercise.php

--- array_exercise.php
+++ array_exercise.php
@@ -28,3 +28,27 @@ krsort($person);
 foreach ($person as $key => $value) {
     echo "$key: $value\n";
 }
+
+
+//Temperature in Array Calculate
+//------------------------------
+//Write a PHP script to calculate and display average temperature, five lowest and highest temperatures.
+$temperature = [78, 60, 62, 68, 71, 68, 73, 85, 66, 64, 76, 63, 75, 76, 73, 68, 62, 73, 72, 65, 74, 62, 62, 65, 64, 68, 73, 75, 79, 73];
+$sum = 0;
+foreach ($temperature as $item) {
+    $sum = $sum + $item;
+}
+$avg_temp = ($sum / count($temperature));
+echo "Average Temperature is: $avg_temp\n";
+//sort array by lowest to Highest
+echo "List of Five lowest temperatures" . PHP_EOL;
+sort($temperature);
+for ($i = 0; $i < 5; $i++) {
+    echo $temperature[$i] . " ";
+}
+echo PHP_EOL;
+echo "List of Five Highest temperatures" . PHP_EOL;
+rsort($temperature);
+for ($i = 0; $i < 5; $i++) {
+    echo $temperature[$i] . " ";
+}
