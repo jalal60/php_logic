3d9fb1accdd06a923fc18f10b6d0eb2261feb3bf
Md. Shahjalal <jalal2k4@gmail.com>
2023-10-11 15:50:46 +0600

    Update string_function_notes.php

--- string_function_notes.php
+++ string_function_notes.php
@@ -81,6 +81,6 @@ echo $newString2;
   // $string="Hello World";
   // $string="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
   // // $parts=str_split($string);
-  // $parts=str_split($string,20); //breaking each 2 character.
+  // $parts=str_split($string,20); //breaking each 20 character.
   // print_r($parts);
   //---------------------------------------
