7b9403fd84d60c9a9a76e05ad429584c46303317
Md. Shahjalal <jalal2k4@gmail.com>
2023-10-11 15:50:25 +0600

    Update string_function_notes.php

--- string_function_notes.php
+++ string_function_notes.php
@@ -76,4 +76,11 @@ echo $newString2;
   // $newString=implode(" ",$stringParts);
   // echo $newString;
   //---------------------------------------
-
+  //When use want to break a senetence/string into small parts of character
+  //then we will use str_split(). which will break each character.
+  // $string="Hello World";
+  // $string="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+  // // $parts=str_split($string);
+  // $parts=str_split($string,20); //breaking each 2 character.
+  // print_r($parts);
+  //---------------------------------------
