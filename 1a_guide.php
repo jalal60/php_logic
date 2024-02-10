019e2926624bc72e7cbc316d245cee68e1988555
Md. Shahjalal <jalal2k4@gmail.com>
2024-02-10 20:14:15 +0600

    Update 1a_guide.php

--- 1a_guide.php
+++ 1a_guide.php
@@ -1,1 +1,38 @@
+<?php
+//////////////////////ALL ABOUT FUNCTION////////////////////////////
+//Declare Restricted Data Type
+declare(strict_types=1);
+//Declare FUnction as multiple Data type.
+function myFun(int|string|null $age, string $name, int $default = 20)
+{
+    echo "Age=" . $age . "Name=" . $name . "Default=" . $default . PHP_EOL;
+}
+
+myFun(null, "Jalal", 50);
+myFun(10, "Kamal");
+
+//Function has return value with typ
+function sum(int $a, int $b): int
+{
+    return $a + $b;
+}
+
+echo sum(25, 40);
+
+
+//Create and Assign Anonyms Function
+$demoFun = function () {
+    $x = 10;
+    $y = 20;
+    echo $x + $y;
+};
+
+//Call Anonyms function
+$demoFun();
+
+//Arrow Function
+$arrowFun = fn ($x, $y) => $x + $y;
+echo $arrowFun(20, 82);
+$arrowFun2 = fn () => 100;
+echo $arrowFun2();
 
