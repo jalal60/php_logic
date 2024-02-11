9d7f7e71ed530e06be67a40ba237744dc5124258
Md. Shahjalal <jalal2k4@gmail.com>
2024-02-11 23:17:52 +0600

    Update 1a_guide.php

--- 1a_guide.php
+++ 1a_guide.php
@@ -43,4 +43,84 @@ hello(function () {
     echo "Hello Callback Function";
 });
 
+/////////////////////////CALL BACK FUNCTION///////////////////////////////
+//Simple declare a callback function.
+function process($callback)
+{
+    $callback();
+}
+process(function () {
+    echo "Hello Callback Again";
+});
+echo PHP_EOL;
+//More About Callback Function
+function add($x, $y)
+{
+    $result = $x + $y;
+    echo "Adding=" . $result . PHP_EOL;
+}
+function sub($x, $y)
+{
+    $result = $x - $y;
+    echo "Subtracting=" . $result . PHP_EOL;
+}
+function myCallback($x, $y, $cb)
+{
+    if (is_callable($cb)) {
+        $cb($x, $y);
+    } else {
+        echo "No Function";
+    }
+}
+myCallback(20, 30, "sub");
+
+//Practical Example of CallBack Function odd even by array_filter()
+$arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+function isEven($n)
+{
+    if ($n % 2 == 0) {
+        return true;
+    } else
+        return false;
+}
+$evenNum = array_filter($arr1, "isEven");
+print_r($evenNum);
+
+//Callback function by finding Male Female by array_filter()
+$persons = [
+    ['name' => 'Md. Shahjalal', 'gender' => 'Male'],
+    ['name' => 'Kamal', 'gender' => 'Male'],
+    ['name' => 'Jamal', 'gender' => 'Male'],
+    ['name' => 'Monika', 'gender' => 'Female'],
+    ['name' => 'Shofiq', 'gender' => 'Male'],
+    ['name' => 'Jui', 'gender' => 'FeMale'],
+];
+
+function isMale($person)
+{
+    if ($person['gender'] == 'Male')
+        return true;
+}
+function isFemale($person)
+{
+    if ($person['gender'] == 'FeMale')
+        return true;
+}
+$result = array_filter($persons, "isMale");
+print_r($result);
+
+//Using array_map callback function
+$number = [1, 2, 3, 4, 5];
+function square($n)
+{
+    return $n * $n;
+}
+function cube($n)
+{
+    return $n * $n * $n;
+}
+$show = array_map("cube", $number);
+print_r($show);
+
 
